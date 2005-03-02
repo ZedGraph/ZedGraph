e00b8e0655cb7ebebb9fc5a5a7f439e4126a3e09
Jerry Vos <jerryvos@users.sourceforge.net>
2005-03-02 19:18:44 +0000

    Cleanup.

--- demo/ZedGraph.Demo/ZedGraphDemo.cs
+++ demo/ZedGraph.Demo/ZedGraphDemo.cs
@@ -1,3 +1,21 @@
+//============================================================================
+//ZedGraph Class Library - A Flexible Line Graph/Bar Graph Library in C#
+//Copyright (C) 2005 Jerry Vos
+//
+//This library is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 2.1 of the License, or (at your option) any later version.
+//
+//This library is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public
+//License along with this library; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//=============================================================================
 using System;
 using Panel	= System.Windows.Forms;
 
@@ -10,6 +28,9 @@ namespace ZedGraph.Demo
 	/// simplifying reading their code and allowing for quick switches 
 	/// between demos.
 	/// </summary>
+	/// 
+	/// <author> Jerry Vos </author>
+	/// <version> $Revision: 1.2 $ $Date: 2005-03-02 19:18:44 $ </version>
 	public interface ZedGraphDemo
 	{
 		/// <summary>
