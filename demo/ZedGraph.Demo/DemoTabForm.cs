15765f318c74c7ede9276cb5f4a1f8708c1946bd
Jerry Vos <jerryvos@users.sourceforge.net>
2005-03-02 19:18:17 +0000

    Code pretification.

--- demo/ZedGraph.Demo/DemoTabForm.cs
+++ demo/ZedGraph.Demo/DemoTabForm.cs
@@ -1,16 +1,40 @@
+//============================================================================
+//ZedGraph Class Library - A Flexible Line Graph/Bar Graph Library in C#
+//Copyright (C) 2005 Jerry Vos
+//
+//This library is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 2.1 of the License, or (at your option) any later version.
+//
+//This library is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public
+//License along with this library; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//=============================================================================
 using System;
 
 namespace ZedGraph.Demo
 {
 	/// <summary>
-	/// Summary description for DemoTabForm.
+	/// An implementation of ChartTabForm that loads up ZedGraph demos.
 	/// </summary>
+	/// 
+	/// <author> Jerry Vos </author>
+	/// <version> $Revision: 1.3 $ $Date: 2005-03-02 19:18:17 $ </version>
 	public class DemoTabForm : ChartTabForm
 	{
+	#region Constructor
 		public DemoTabForm()
 		{
 		}
+	#endregion
 
+	#region Methods
 		protected override void loadDemos()
 		{
 			loadDemo(new SimpleDemo());
@@ -45,11 +69,14 @@ namespace ZedGraph.Demo
 			// TODO: add more demos here
 			// ...
 		}
+	#endregion
 
+	#region Main
 		[STAThread]
 		static void Main() 
 		{
 			System.Windows.Forms.Application.Run(new DemoTabForm());
 		}
+	#endregion
 	}
 }
