51ab366110bea9f511ab7ec0c2e4f19cd930d29f
John Champion <jchampion@users.sourceforge.net>
2005-03-16 04:04:06 +0000

    update webdemo

--- source/ZedGraph/ZedGraphWeb.cs
+++ source/ZedGraph/ZedGraphWeb.cs
@@ -38,7 +38,7 @@ namespace ZedGraph
 	/// property.
 	/// </summary>
 	/// <author> Darren Martz  revised by John Champion </author>
-	/// <version> $Revision: 3.19 $ $Date: 2005-03-15 19:20:13 $ </version>
+	/// <version> $Revision: 3.20 $ $Date: 2005-03-16 04:04:06 $ </version>
 	[	
 	ParseChildren(true),
 	PersistChildren(false),
@@ -113,103 +113,146 @@ namespace ZedGraph
 		/// <param name="pane">A reference to the <see cref="GraphPane"/></param>
 		static public void RenderDemo( Graphics g, ZedGraph.GraphPane pane )
 		{
+			// Set the titles and axis labels
+			pane.Title = "Wacky Widget Company\nProduction Report";
+			pane.XAxis.Title = "Time, Days\n(Since Plant Construction Startup)";
+			pane.YAxis.Title = "Widget Production\n(units/hour)";
+			
+			LineItem curve;
+
+			// Set up curve "Larry"
 			double[] x = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
 			double[] y = { 20, 10, 50, 25, 35, 75, 90, 40, 33, 50 };
-			LineItem curve;
+			// Use green, with circle symbols
 			curve = pane.AddCurve( "Larry", x, y, Color.Green, SymbolType.Circle );
 			curve.Line.Width = 1.5F;
+			// Fill the area under the curve with a white-green gradient
 			curve.Line.Fill = new Fill( Color.White, Color.FromArgb( 60, 190, 50), 90F );
+			// Make it a smooth line
 			curve.Line.IsSmooth = true;
 			curve.Line.SmoothTension = 0.6F;
+			// Fill the symbols with white
 			curve.Symbol.Fill = new Fill( Color.White );
 			curve.Symbol.Size = 10;
-
+			
+			// Second curve is "moe"
 			double[] x3 = { 150, 250, 400, 520, 780, 940 };
 			double[] y3 = { 5.2, 49.0, 33.8, 88.57, 99.9, 36.8 };
+			// Use a red color with triangle symbols
 			curve = pane.AddCurve( "Moe", x3, y3, Color.FromArgb( 200, 55, 135), SymbolType.Triangle );
 			curve.Line.Width = 1.5F;
-			curve.Symbol.Fill = new Fill( Color.White );
+			// Fill the area under the curve with semi-transparent pink using the alpha value
 			curve.Line.Fill = new Fill( Color.White, Color.FromArgb( 160, 230, 145, 205), 90F );
+			// Fill the symbols with white
+			curve.Symbol.Fill = new Fill( Color.White );
 			curve.Symbol.Size = 10;
-
+			
+			// Third Curve is a bar, called "Wheezy"
 			double[] x4 = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
 			double[] y4 = { 30, 45, 53, 60, 75, 83, 84, 79, 71, 57 };
 			BarItem bar = pane.AddBar( "Wheezy", x4, y4, Color.SteelBlue );
+			// Fill the bars with a RosyBrown-White-RosyBrown gradient
 			bar.Bar.Fill = new Fill( Color.RosyBrown, Color.White, Color.RosyBrown );
-			pane.ClusterScaleWidth = 100;
-			pane.BarType = BarType.Stack;
 
+			// Fourth curve is a bar
 			double[] x2 = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
 			double[] y2 = { 10, 15, 17, 20, 25, 27, 29, 26, 24, 18 };
 			bar = pane.AddBar( "Curly", x2, y2, Color.RoyalBlue );
+			// Fill the bars with a RoyalBlue-White-RoyalBlue gradient
 			bar.Bar.Fill = new Fill( Color.RoyalBlue, Color.White, Color.RoyalBlue );
-			pane.ClusterScaleWidth = 100;
-
-			pane.PaneFill = new Fill( Color.WhiteSmoke, Color.Lavender, 0F );			
-
+			
+			// Fill the pane background with a gradient
+			pane.PaneFill = new Fill( Color.WhiteSmoke, Color.Lavender, 0F );
+			// Fill the axis background with a gradient
 			pane.AxisFill = new Fill( Color.FromArgb( 255, 255, 245),
 				Color.FromArgb( 255, 255, 190), 90F );
+			
 
+			// Make each cluster 100 user scale units wide.  This is needed because the X Axis
+			// type is Linear rather than Text or Ordinal
+			pane.ClusterScaleWidth = 100;
+			// Bars are stacked
+			pane.BarType = BarType.Stack;
+
+			// Enable the X and Y axis grids
 			pane.XAxis.IsShowGrid = true;
 			pane.YAxis.IsShowGrid = true;
-			pane.YAxis.Max = 120;
 
+			// Manually set the scale maximums according to user preference
+			pane.XAxis.Max = 1200;
+			pane.YAxis.Max = 120;
+			
+			// Add a text item to decorate the graph
 			TextItem text = new TextItem("First Prod\n21-Oct-93", 175F, 80.0F );
+			// Align the text such that the Bottom-Center is at (175, 80) in user scale coordinates
 			text.Location.AlignH = AlignH.Center;
 			text.Location.AlignV = AlignV.Bottom;
 			text.FontSpec.Fill = new Fill( Color.White, Color.PowderBlue, 45F );
+			text.FontSpec.StringAlignment = StringAlignment.Near;
 			pane.GraphItemList.Add( text );
 
+			// Add an arrow pointer for the above text item
 			ArrowItem arrow = new ArrowItem( Color.Black, 12F, 175F, 77F, 100F, 45F );
 			arrow.Location.CoordinateFrame = CoordType.AxisXYScale;
 			pane.GraphItemList.Add( arrow );
 
+			// Add a another text item to to point out a graph feature
 			text = new TextItem("Upgrade", 700F, 50.0F );
+			// rotate the text 90 degrees
 			text.FontSpec.Angle = 90;
-			text.FontSpec.FontColor = Color.Black;
+			// Align the text such that the Right-Center is at (700, 50) in user scale coordinates
 			text.Location.AlignH = AlignH.Right;
 			text.Location.AlignV = AlignV.Center;
+			// Disable the border and background fill options for the text
 			text.FontSpec.Fill.IsVisible = false;
 			text.FontSpec.Border.IsVisible = false;
 			pane.GraphItemList.Add( text );
 
+			// Add an arrow pointer for the above text item
 			arrow = new ArrowItem( Color.Black, 15, 700, 53, 700, 80 );
 			arrow.Location.CoordinateFrame = CoordType.AxisXYScale;
 			arrow.PenWidth = 2.0F;
 			pane.GraphItemList.Add( arrow );
 
+			// Add a text "Confidential" stamp to the graph
 			text = new TextItem("Confidential", 0.85F, -0.03F );
+			// use AxisFraction coordinates so the text is placed relative to the AxisRect
 			text.Location.CoordinateFrame = CoordType.AxisFraction;
-
+			// rotate the text 15 degrees
 			text.FontSpec.Angle = 15.0F;
+			// Text will be red, bold, and 16 point
 			text.FontSpec.FontColor = Color.Red;
 			text.FontSpec.IsBold = true;
 			text.FontSpec.Size = 16;
+			// Disable the border and background fill options for the text
 			text.FontSpec.Border.IsVisible = false;
-			text.FontSpec.Border.Color = Color.Red;
 			text.FontSpec.Fill.IsVisible = false;
-
+			// Align the text such the the Left-Bottom corner is at the specified coordinates
 			text.Location.AlignH = AlignH.Left;
 			text.Location.AlignV = AlignV.Bottom;
 			pane.GraphItemList.Add( text );
 
+			// Add a BoxItem to show a colored band behind the graph data
 			BoxItem box = new BoxItem( new RectangleF( 0, 110, 1200, 10 ),
 				Color.Empty, Color.FromArgb( 225, 245, 225) );
 			box.Location.CoordinateFrame = CoordType.AxisXYScale;
+			// Align the left-top of the box to (0, 110)
 			box.Location.AlignH = AlignH.Left;
 			box.Location.AlignV = AlignV.Top;
+			// place the box behind the axis items, so the grid is drawn on top of it
 			box.ZOrder = ZOrder.E_BehindAxis;
 			pane.GraphItemList.Add( box );
-
-			text = new TextItem( "Peak Range", 1170, 105 );
-			text.Location.CoordinateFrame = CoordType.AxisXYScale;
-			text.Location.AlignH = AlignH.Right;
-			text.Location.AlignV = AlignV.Center;
-			text.FontSpec.IsItalic = true;
-			text.FontSpec.IsBold = false;
-			text.FontSpec.Fill.IsVisible = false;
-			text.FontSpec.Border.IsVisible = false;
-			pane.GraphItemList.Add( text );
+			
+			// Add some text inside the above box to indicate "Peak Range"
+			TextItem myText = new TextItem( "Peak Range", 1170, 105 );
+			myText.Location.CoordinateFrame = CoordType.AxisXYScale;
+			myText.Location.AlignH = AlignH.Right;
+			myText.Location.AlignV = AlignV.Center;
+			myText.FontSpec.IsItalic = true;
+			myText.FontSpec.IsBold = false;
+			myText.FontSpec.Fill.IsVisible = false;
+			myText.FontSpec.Border.IsVisible = false;
+			pane.GraphItemList.Add( myText );
 
 			pane.AxisChange( g );
 		}
