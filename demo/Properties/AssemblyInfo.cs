7205a1b0796fb684618acc4525c36cce3b8fdcb9
John Champion <jchampion@users.sourceforge.net>
2006-09-09 18:22:47 +0000

    Version 5.0.2

--- demo/Properties/AssemblyInfo.cs
+++ demo/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "5.0.1.*" )]
+[assembly: AssemblyVersion( "5.0.2.*" )]
 //[assembly: AssemblyFileVersion( "1.0.0.0" )]
