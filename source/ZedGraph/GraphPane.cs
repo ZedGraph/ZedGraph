14ff562582c5dd57cd933707cbc037cb5f499c9b
Nathan Hartman <nathanial.hartman@gmail.com>
2012-11-05 16:25:43 -0600

    turned on antialias in demo, turned off antialias on borders and ticks, switched to cleartype rendering for antialias font

--- source/ZedGraph/GraphPane.cs
+++ source/ZedGraph/GraphPane.cs
@@ -814,8 +814,11 @@ namespace ZedGraph
 				_graphObjList.Draw( g, this, scaleFactor, ZOrder.C_BehindChartBorder );
 			}
 
+		    var smode = g.SmoothingMode;
+            g.SmoothingMode = SmoothingMode.None;
 			// Border the axis itself
 			_chart.Border.Draw( g, this, scaleFactor, _chart._rect );
+		    g.SmoothingMode = smode;
 
 			if ( showGraf )
 			{
