160ec1bc5e5c34e78bcf81385e4c81fbae370673
Warren Kaufman <discomurray@gmail.com>
2018-05-20 10:20:53 -0600

    Seperate WinForms components to seperate package
    To keep the API the same between .net standard and framework packages,
    as well as to support more UI frameworks (UWP, WPF) the WinForms
    components have been moved to their own package.

---
+++ source/ZedGraph.WinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("ZedGraph Library")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ae4a186-e416-41ab-9dcd-373980552e55")]
+
+[assembly: NeutralResourcesLanguage("")]
+
+[assembly: CLSCompliant(true)]
--- source/ZedGraph/Properties/AssemblyInfo.cs
+++ source/ZedGraph/Properties/AssemblyInfo.cs
@@ -17,6 +17,13 @@ using System;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible( false )]
 
+[assembly: InternalsVisibleTo("ZedGraph.WinForms, PublicKey=" +
+    "00240000048000009400000006020000002400005253413100040000010001002f86fc317ec78e" +
+    "545188db70d915056f5a42bcc59f3e973eea23b771b6e6778c74d5b426a9ff9eb73447edee78fc" +
+    "45ce3801471ae8070b846df0d8e327f2c9f493de3a761f1f6c2cab157cb4e64a7f0df833c95648" +
+    "7dded5cb3fb80e104a0ba17b9cbd23eb3974e1724831f0fdcc431f4c08b7679ad3a1212fbcb173" +
+    "bee104be")]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid( "a552bf32-72a3-4d27-968c-72e7a90243f2" )]
 
